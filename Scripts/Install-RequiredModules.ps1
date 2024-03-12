a9c2b438a57e8c1ce91db86f8228a5363771ee73
Kristian Bentsen Kjærgård (KBK) <kbk@complea.dk>
2024-03-12 10:30:25 +0100

    Formatting

--- Scripts/Install-RequiredModules.ps1
+++ Scripts/Install-RequiredModules.ps1
@@ -1,26 +1,25 @@
 # Checks if modules in the list is installed and install if it's not.
 $Modules = @(
-    "ExchangeOnlineManagement",
-    "MSOnline",
-    "Microsoft.Graph",
-    "Microsoft.Graph.Beta",
-    "AzureAD",
-    "AIPService",
-    "ImportExcel",
-    "MicrosoftTeams",
-    "PnP.PowerShell"
+    'ExchangeOnlineManagement',
+    'MSOnline',
+    'Microsoft.Graph',
+    'Microsoft.Graph.Beta',
+    'AzureAD',
+    'AIPService',
+    'ImportExcel',
+    'MicrosoftTeams',
+    'PnP.PowerShell'
 )
 function Install-RequiredModules {
     param (
         [parameter(Mandatory = $true)][System.Array]$Modules
     )
     # Set PSGallery to trusted
-    if ((Get-PSRepository -Name 'PSGallery').InstallationPolicy -eq "Trusted") {
-        Write-Host "PSGallery already trusted"
-    }
-    else {
-        Write-Host "PSGallery not trusted, setting it to trusted. Please wait..." -ForegroundColor Yellow
-        Write-Host "Trusting PS Gallery" -ForegroundColor Yellow
+    if ((Get-PSRepository -Name 'PSGallery').InstallationPolicy -eq 'Trusted') {
+        Write-Host 'PSGallery already trusted'
+    } else {
+        Write-Host 'PSGallery not trusted, setting it to trusted. Please wait...' -ForegroundColor Yellow
+        Write-Host 'Trusting PS Gallery' -ForegroundColor Yellow
         Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
     }
 
@@ -46,8 +45,7 @@ function Install-RequiredModules {
                         Uninstall-Module $module -MaximumVersion $InstalledModule.Version -ErrorAction Stop
                         Write-Host "Success. Old version $($InstalledModule.Version) of $Module uninstalled." -ForegroundColor Green
                         
-                    }
-                    catch {
+                    } catch {
                         Write-Host "ERROR. Old version $($InstalledModule.Version) of $Module was not uninstalled." -ForegroundColor Red
                         Write-Host "Please uninstall the module manually with: Uninstall-Module $Module -MaximumVersion $($InstalledModule.Version)"
                     }
@@ -55,18 +53,15 @@ function Install-RequiredModules {
                 catch {
                     Write-Host "Could not update $Module. Please update it manually with: Update-Module $Module" -ForegroundColor Red
                 }
-            }
-            else {
+            } else {
                 Write-Host "$Module module is up to date. Moving on." -ForegroundColor Green
             }
-        }
-        else {
+        } else {
             Write-Host "$Module module is not installed. Installing module..." -ForegroundColor Yellow
             try {
                 Install-Module $Module -ErrorAction Stop
                 Write-Host "$Module sucessfully installed." -ForegroundColor Green
-            }
-            catch {
+            } catch {
                 Write-Host "Could not install $Module. Please install it manually with: Install-Module $Module and rerun the script." -ForegroundColor Red
                 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
                 Exit
